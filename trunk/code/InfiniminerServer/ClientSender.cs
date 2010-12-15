d16a3b35e589920944bf6273e01e2d91231bb96f
oedsdeboer1991@gmail.com <oedsdeboer1991@gmail.com@e9e50cd2-f091-340c-55ec-e343509d7995>
2010-12-15 13:34:09 +0000

    Adding the source
    This is now pre-alpha its buggy and its just infiniminer with a bit updates

---
+++ trunk/code/InfiniminerServer/ClientSender.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lidgren.Network;
+using Lidgren.Network.Xna;
+using Microsoft.Xna.Framework;
+ 
+public class qmsg
+{
+    public NetBuffer Buffer;
+    public NetChannel Channel;
+    public qmsg(NetBuffer buff, NetChannel chan)
+    {
+        Buffer = buff;
+        Channel = chan;
+    }
+    
+}
+namespace MineWorld
+{
+
+   
+    public class IClient : Player
+    {
+        public System.Collections.ObjectModel.Collection<qmsg> Que;
+        private System.Collections.ObjectModel.Collection<qmsg> InternalQue;
+        public NetConnection Connection;
+        public bool Active = true;
+        public System.Threading.EventWaitHandle WH;
+        int msgs = 0;
+        //int qmsgs = 0;
+
+        public IClient(NetConnection conn, Game gameInstance):base(conn,gameInstance)
+        {
+            Que = new System.Collections.ObjectModel.Collection<qmsg>();
+            InternalQue = new System.Collections.ObjectModel.Collection<qmsg>();
+            Connection = conn;
+            WH = new System.Threading.AutoResetEvent(false);
+
+        }
+        public void start()
+        {
+            while(Active)
+            {
+                if (msgs > 0)
+                {
+                    if (Connection.Status == NetConnectionStatus.Connected)
+                    {
+                        qmsg[] temp;
+                        lock (Que)
+                        {
+                            temp = new qmsg[Que.Count];
+                            Que.CopyTo(temp, 0);
+                            Que.Clear();
+                            msgs = 0;
+                        }
+                        for (int i = 0; i < temp.GetLength(0); i++)
+                        {
+
+                            Connection.SendMessage(temp[i].Buffer, temp[i].Channel);
+
+                        }
+                    }
+                    else
+                    {
+                        Active = false;
+                    }
+                }
+                if (msgs == 0)
+                {
+                    //System.Threading.Thread.Sleep(25);
+                    WH.WaitOne();
+                }
+            }
+        }
+        public void AddQueMsg(NetBuffer buff, NetChannel chan)
+        {
+            lock (Que)
+            {
+                Que.Add(new qmsg(buff, chan));
+                msgs++;
+            }
+            WH.Set();
+        }
+    }
+
+    
+}
---
+++ trunk/code/InfiniminerServer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace MineWorld.Server
+{
+    class Program
+    {
+        static void RunServer()
+        {
+            bool restartServer = true;
+            while (restartServer)
+            {
+                MineWorldServer MineWorldServer = new MineWorldServer();
+                restartServer = MineWorldServer.Start();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                RunServer();
+            }
+            else
+            {
+                try
+                {
+                    RunServer();
+                }
+                catch (Exception e)
+                {
+                    System.Windows.Forms.MessageBox.Show(e.Message + "\r\n\r\n" + e.StackTrace);
+                }
+            }
+        }
+    }
+}
