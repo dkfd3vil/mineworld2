cf6f429c19c75522f428725bcca270408085c2e6
oedsdeboer1991@gmail.com <oedsdeboer1991@gmail.com@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-02-09 09:27:52 +0000

    Creating a new branch for Martijn to work in

---
+++ branches/Martijnbranch/InfiniminerServer/InfiniminerNetServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Lidgren.Network;
+using Lidgren.Network.Xna;
+using Microsoft.Xna.Framework;
+
+namespace MineWorld
+{
+    public class MineWorldNetServer : NetServer
+    {
+        public MineWorldNetServer(NetConfiguration config)
+            : base(config)
+        {
+        }
+
+        /* crappy hack to fix duplicate key error crash in Lidgren, hopefully a new
+         * version of Lidgren will fix this issue. */
+        public bool SanityCheck(NetConnection connection)
+        {
+            if (this.m_connections.Contains(connection) == false)
+            {
+                if (this.m_connectionLookup.ContainsKey(connection.RemoteEndpoint))
+                {
+                    this.m_connectionLookup.Remove(connection.RemoteEndpoint);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
