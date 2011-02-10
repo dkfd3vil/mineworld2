882e0603955962da2c38210895d3ffaf74d7d8f7
oedsdeboer1991@gmail.com <oedsdeboer1991@gmail.com@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-02-10 07:49:26 +0000

    Merged oedsbranch into trunk, This is our working directory now

---
+++ InfiniminerServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MineWorld.Server
+{
+    class Program
+    {
+        static void RunServer()
+        {
+            bool restartServer = true;
+            while (restartServer)
+            {
+                MineWorldServer MineWorldServer = new MineWorldServer();
+                restartServer = MineWorldServer.Start();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                RunServer();
+            }
+            else
+            {
+                try
+                {
+                    RunServer();
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message + "\r\n\r\n" + e.StackTrace);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.ReadLine();
+                }
+            }
+        }
+
+    }
+}
