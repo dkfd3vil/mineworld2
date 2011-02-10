10b155b95e4363b327f028fc9a0604c5a49aaa6c
pizzamartijn <pizzamartijn@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-02-10 11:07:05 +0000

    createPublicBuild.bat updated to include all content in the build

--- trunk/createPublicBuild.bat
+++ trunk/createPublicBuild.bat
@@ -10,4 +10,4 @@ mkdir PublicBuild\ServerConfigs
 mkdir PublicBuild\Content
 copy Output\debug\ClientConfigs\* PublicBuild\ClientConfigs\
 copy Output\debug\ServerConfigs\* PublicBuild\ServerConfigs\
-copy Output\debug\Content\* PublicBuild\Content\
+xcopy Output\debug\Content\* PublicBuild\Content\ /S
