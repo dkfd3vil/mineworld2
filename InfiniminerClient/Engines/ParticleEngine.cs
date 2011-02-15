34c6124539c59c5b5d870a8d347b91178ed76db8
pizzamartijn <pizzamartijn@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-02-15 10:58:31 +0000

    Changed ParticleEngine behavior

--- InfiniminerClient/Engines/ParticleEngine.cs
+++ InfiniminerClient/Engines/ParticleEngine.cs
@@ -16,7 +16,6 @@ namespace MineWorld
     public class Particle
     {
         public Vector3 Position;
-        public Vector3 Velocity;
         public float Size;
         public Color Color;
         public int StepsToLife=60;
@@ -48,7 +47,7 @@ namespace MineWorld
 
         private VertexPositionTextureShade[] GenerateVertices()
         {
-            VertexPositionTextureShade[] cubeVerts = new VertexPositionTextureShade[36];
+            VertexPositionTextureShade[] cubeVerts = new VertexPositionTextureShade[6];
 
             // BOTTOM
             cubeVerts[0] = new VertexPositionTextureShade(new Vector3(-1, -1, -1), new Vector2(0, 0), 0.3);
@@ -57,47 +56,6 @@ namespace MineWorld
             cubeVerts[3] = new VertexPositionTextureShade(new Vector3(1, -1, -1), new Vector2(0, 0), 0.3);
             cubeVerts[4] = new VertexPositionTextureShade(new Vector3(1, 1, -1), new Vector2(0, 0), 0.3);
             cubeVerts[5] = new VertexPositionTextureShade(new Vector3(-1, 1, -1), new Vector2(0, 0), 0.3);
-
-            // TOP
-            cubeVerts[30] = new VertexPositionTextureShade(new Vector3(-1, -1, 1), new Vector2(0, 0), 1.0);
-            cubeVerts[31] = new VertexPositionTextureShade(new Vector3(1, -1, 1), new Vector2(0, 0), 1.0);
-            cubeVerts[32] = new VertexPositionTextureShade(new Vector3(-1, 1, 1), new Vector2(0, 0), 1.0);
-            cubeVerts[33] = new VertexPositionTextureShade(new Vector3(1, -1, 1), new Vector2(0, 0), 1.0);
-            cubeVerts[34] = new VertexPositionTextureShade(new Vector3(1, 1, 1), new Vector2(0, 0), 1.0);
-            cubeVerts[35] = new VertexPositionTextureShade(new Vector3(-1, 1, 1), new Vector2(0, 0), 1.0);
-
-            // LEFT
-            cubeVerts[6] = new VertexPositionTextureShade(new Vector3(-1, -1, -1), new Vector2(0, 0), 0.7);
-            cubeVerts[7] = new VertexPositionTextureShade(new Vector3(-1, -1, 1), new Vector2(0, 0), 0.7);
-            cubeVerts[8] = new VertexPositionTextureShade(new Vector3(-1, 1, 1), new Vector2(0, 0), 0.7);
-            cubeVerts[9] = new VertexPositionTextureShade(new Vector3(-1, -1, -1), new Vector2(0, 0), 0.7);
-            cubeVerts[10] = new VertexPositionTextureShade(new Vector3(-1, 1, 1), new Vector2(0, 0), 0.7);
-            cubeVerts[11] = new VertexPositionTextureShade(new Vector3(-1, 1, -1), new Vector2(0, 0), 0.7);
-
-            // RIGHT
-            cubeVerts[12] = new VertexPositionTextureShade(new Vector3(1, -1, -1), new Vector2(0, 0), 0.7);
-            cubeVerts[13] = new VertexPositionTextureShade(new Vector3(1, -1, 1), new Vector2(0, 0), 0.7);
-            cubeVerts[14] = new VertexPositionTextureShade(new Vector3(1, 1, 1), new Vector2(0, 0), 0.7);
-            cubeVerts[15] = new VertexPositionTextureShade(new Vector3(1, -1, -1), new Vector2(0, 0), 0.7);
-            cubeVerts[16] = new VertexPositionTextureShade(new Vector3(1, 1, 1), new Vector2(0, 0), 0.7);
-            cubeVerts[17] = new VertexPositionTextureShade(new Vector3(1, 1, -1), new Vector2(0, 0), 0.7);
-
-            // FRONT
-            cubeVerts[18] = new VertexPositionTextureShade(new Vector3(-1, 1, -1), new Vector2(0, 0), 0.5);
-            cubeVerts[19] = new VertexPositionTextureShade(new Vector3(-1, 1, 1), new Vector2(0, 0), 0.5);
-            cubeVerts[20] = new VertexPositionTextureShade(new Vector3(1, 1, 1), new Vector2(0, 0), 0.5);
-            cubeVerts[21] = new VertexPositionTextureShade(new Vector3(-1, 1, -1), new Vector2(0, 0), 0.5);
-            cubeVerts[22] = new VertexPositionTextureShade(new Vector3(1, 1, 1), new Vector2(0, 0), 0.5);
-            cubeVerts[23] = new VertexPositionTextureShade(new Vector3(1, 1, -1), new Vector2(0, 0), 0.5);
-
-            // BACK
-            cubeVerts[24] = new VertexPositionTextureShade(new Vector3(-1, -1, -1), new Vector2(0, 0), 0.5);
-            cubeVerts[25] = new VertexPositionTextureShade(new Vector3(-1, -1, 1), new Vector2(0, 0), 0.5);
-            cubeVerts[26] = new VertexPositionTextureShade(new Vector3(1, -1, 1), new Vector2(0, 0), 0.5);
-            cubeVerts[27] = new VertexPositionTextureShade(new Vector3(-1, -1, -1), new Vector2(0, 0), 0.5);
-            cubeVerts[28] = new VertexPositionTextureShade(new Vector3(1, -1, 1), new Vector2(0, 0), 0.5);
-            cubeVerts[29] = new VertexPositionTextureShade(new Vector3(1, -1, -1), new Vector2(0, 0), 0.5);
-
             return cubeVerts;
         }
 
@@ -113,8 +71,7 @@ namespace MineWorld
 
             foreach (Particle p in particleList)
             {
-                p.Position += (float)gameTime.ElapsedGameTime.TotalSeconds * p.Velocity;
-                p.Velocity.Y -= 8 * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                p.Position.Y += (float)gameTime.ElapsedGameTime.TotalSeconds;
                 if (_P.blockEngine.SolidAtPoint(p.Position))
                     p.FlaggedForDeletion = true;
                 if (p.StepsToLife < 1)
@@ -131,19 +88,14 @@ namespace MineWorld
 
         public void CreateExplosionDebris(Vector3 explosionPosition)
         {
-            for (int i = 0; i < 50; i++)
-            {
-                Particle p = new Particle();
-                p.Color = new Color(90,60,40);
-                p.Size = (float)(randGen.NextDouble() * 0.4 + 0.05);
-                p.Position = explosionPosition;
-                p.Position.Y += (float)randGen.NextDouble() - 0.5f;
-                p.Velocity = new Vector3((float)randGen.NextDouble() * 8 - 4, (float)randGen.NextDouble() * 8, (float)randGen.NextDouble() * 8 - 4);
-                particleList.Add(p);
-            }
+            Particle p = new Particle();
+            p.Color = new Color(128,128,128);
+            p.Size = 0.4f;
+            p.Position = explosionPosition;
+            particleList.Add(p);
         }
 
-        public void CreateBloodSplatter(Vector3 playerPosition, Color color)
+        /*public void CreateBloodSplatter(Vector3 playerPosition, Color color)
         {
             for (int i = 0; i < 30; i++)
             {
@@ -155,7 +107,7 @@ namespace MineWorld
                 p.Velocity = new Vector3((float)randGen.NextDouble() * 5 - 2.5f, (float)randGen.NextDouble() * 4f, (float)randGen.NextDouble() * 5 - 2.5f);
                 particleList.Add(p);
             }
-        }
+        }*/
 
         public void Render(GraphicsDevice graphicsDevice)
         {
