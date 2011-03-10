56950b7b63b9d0124fc9beb2a8fcc2d8f3e749e5
oedsdeboer1991@gmail.com <oedsdeboer1991@gmail.com@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-03-10 08:05:17 +0000

    Trunk updated to r129

---
+++ InfiniminerServer/LogSystem.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace MineWorld
+{
+    public partial class MineWorldServer
+    {
+        public void LogWrite(string consoletext)
+        {
+            DateTime dt = DateTime.Now;
+
+            string text = "[" + GetTime(true) + "] ";
+
+            try
+            {
+                if (!File.Exists(Ssettings.LogsDir + "/" + GetDate() + ".txt"))
+                {
+                    FileStream fs = File.Create(Ssettings.LogsDir + "/" + GetDate() + ".txt");
+                    fs.Close();
+                }
+                FileStream file = new FileStream(Ssettings.LogsDir + "/" + GetDate() + ".txt", FileMode.Append, FileAccess.Write);
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine(text + consoletext);
+                sw.Close();
+                file.Close();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public string GetTime(bool usedots)
+        {
+            String DateFormat = "";
+            if (usedots)
+            {
+                DateFormat = "hh:mm:ss";
+            }
+            else
+            {
+                DateFormat = "hh-mm-ss";
+            }
+            return DateTime.Now.ToString(DateFormat);
+        }
+        public string GetDate()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd");
+        }
+    }
+}
---
+++ InfiniminerServer/MapSender.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Lidgren.Network;
+using System.Threading;
+
+namespace MineWorld
+{
+    class MapSender
+    {
+        NetConnection client;
+        Mapsize mapsize;
+        Thread conn;
+        MineWorld.MineWorldServer infs;
+        MineWorld.MineWorldNetServer infsN;
+        //int MAPSIZE = 64;
+        //bool compression = false;
+        //bool finished = false;
+        public bool finished
+        {
+            get
+            {
+                return !conn.IsAlive;
+            }
+        }
+
+        public MapSender(NetConnection nClient, MineWorld.MineWorldServer nInfs, MineWorld.MineWorldNetServer nInfsN,Mapsize mpsize)
+        {
+            client = nClient;
+            infs = nInfs;
+            infsN = nInfsN;
+            mapsize = mpsize;
+            //MAPSIZE = nMAPSIZE;
+            //compression = compress;
+            //finished = false;
+            conn = new Thread(new ThreadStart(this.start));
+            conn.Start();
+            DateTime started = DateTime.Now;
+            TimeSpan diff = DateTime.Now - started;
+            while (!conn.IsAlive&&diff.Milliseconds<250) //Hold execution until it starts
+            {
+                diff = DateTime.Now - started;
+            }
+        }
+
+        private void start()
+        {
+            //Debug.Assert(MAPSIZE == 64, "The BlockBulkTransfer message requires a map size of 64.");
+
+            for (byte x = 0; x < Defines.MAPSIZE; x++)
+                for (byte y = 0; y < Defines.MAPSIZE; y += Defines.PACKETSIZE)
+                {
+                    NetBuffer msgBuffer = infsN.CreateBuffer();
+                    msgBuffer.Write((byte)MineWorld.MineWorldMessage.BlockBulkTransfer);
+                    //msgBuffer.Write((byte)mapsize);
+                    //if (!compression)
+                    //{
+                    msgBuffer.Write(x);
+                    msgBuffer.Write(y);
+                    for (byte dy = 0; dy < Defines.PACKETSIZE; dy++)
+                    {
+                        for (byte z = 0; z < Defines.MAPSIZE; z++)
+                        {
+                            msgBuffer.Write((byte)(infs.blockList[x, y + dy, z]));
+                        }
+                    }
+                    if (client.Status == NetConnectionStatus.Connected)
+                    {
+                        infsN.SendMessage(msgBuffer, client, NetChannel.ReliableUnordered);
+                    }
+                    //}
+                    //TODO: Compression needs to be defaulted i guess
+                    /*
+                    else
+                    {
+                        //Compress the data so we don't use as much bandwith - Xeio's work
+                        var compressedstream = new System.IO.MemoryStream();
+                        var uncompressed = new System.IO.MemoryStream();
+                        var compresser = new System.IO.Compression.GZipStream(compressedstream, System.IO.Compression.CompressionMode.Compress);
+
+                        //Send a byte indicating that yes, this is compressed
+                        msgBuffer.Write((byte)255);
+
+                        //Write everything we want to compress to the uncompressed stream
+                        uncompressed.WriteByte(x);
+                        uncompressed.WriteByte(y);
+
+                        for (byte dy = 0; dy < 16; dy++)
+                            for (byte z = 0; z < MAPSIZE; z++)
+                                uncompressed.WriteByte((byte)(infs.blockList[x, y + dy, z]));
+
+                        //Compress the input
+                        compresser.Write(uncompressed.ToArray(), 0, (int)uncompressed.Length);
+                        //infs.ConsoleWrite("Sending compressed map block, before: " + uncompressed.Length + ", after: " + compressedstream.Length);
+                        compresser.Close();
+
+                        //Send the compressed data
+                        msgBuffer.Write(compressedstream.ToArray());
+                        if (client.Status == NetConnectionStatus.Connected)
+                            infsN.SendMessage(msgBuffer, client, NetChannel.ReliableUnordered);
+                    }
+                     */
+                }
+            conn.Abort();
+        }
+
+        public void stop()
+        {
+            conn.Abort();
+        }
+    }
+}
