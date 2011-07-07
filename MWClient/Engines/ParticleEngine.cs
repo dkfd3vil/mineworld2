c283785700980f6964be60b054da9ec3010ec538
pizzamartijn <pizzamartijn@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-07-07 20:41:00 +0000

    All engines working now

--- MWClient/Engines/ParticleEngine.cs
+++ MWClient/Engines/ParticleEngine.cs
@@ -40,9 +40,9 @@ namespace MineWorld
             randGen = new Random();
             particleList = new List<Particle>();
 
-            vertexDeclaration = new VertexDeclaration(gameInstance.GraphicsDevice, VertexPositionTextureShade.VertexElements);
+            vertexDeclaration = new VertexDeclaration(new VertexElement[0]);
             VertexPositionTextureShade[] vertices = GenerateVertices();
-            vertexBuffer = new VertexBuffer(gameInstance.GraphicsDevice, vertices.Length * VertexPositionTextureShade.SizeInBytes, BufferUsage.WriteOnly);
+            vertexBuffer = new VertexBuffer(gameInstance.GraphicsDevice,typeof(VertexPositionTextureShade), vertices.Length, BufferUsage.WriteOnly);
             vertexBuffer.SetData(vertices);
         }
 
@@ -171,16 +171,11 @@ namespace MineWorld
                 particleEffect.Parameters["xView"].SetValue(_P.playerCamera.ViewMatrix);
                 particleEffect.Parameters["xProjection"].SetValue(_P.playerCamera.ProjectionMatrix);
                 particleEffect.Parameters["xColor"].SetValue(p.Color.ToVector4());
-                particleEffect.Begin();
-                particleEffect.Techniques[0].Passes[0].Begin();
+                particleEffect.Techniques[0].Passes[0].Apply();
 
-                graphicsDevice.RenderState.CullMode = CullMode.None;
-                graphicsDevice.VertexDeclaration = vertexDeclaration;
-                graphicsDevice.Vertices[0].SetSource(vertexBuffer, 0, VertexPositionTextureShade.SizeInBytes);
-                graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, vertexBuffer.SizeInBytes / VertexPositionTextureShade.SizeInBytes / 3);
+                graphicsDevice.SetVertexBuffer(vertexBuffer);
+                graphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, vertexBuffer.VertexCount);
 
-                particleEffect.Techniques[0].Passes[0].End();
-                particleEffect.End();
             }
         }
     }
