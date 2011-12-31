045a6cbecddf668f704385e249751348ac49b32b
oedsdeboer1991@gmail.com <oedsdeboer1991@gmail.com@e9e50cd2-f091-340c-55ec-e343509d7995>
2011-12-31 00:02:30 +0000

    Trying out a gui libary for xna pretty promising so far

--- MineWorldClient/MineWorldClient/GameStates/TitleState.cs
+++ MineWorldClient/MineWorldClient/GameStates/TitleState.cs
@@ -5,27 +5,42 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
+using Ruminate.GUI.Content;
+using Ruminate.GUI.Framework;
 
 namespace MineWorld
 {
     class TitleState : BaseState
     {
         GameStateManager gamemanager;
+        RuminateGUI gui;
+        Button start;
+        ScrollBars bar;
 
         public TitleState(GameStateManager manager, GameStates associatedState)
             : base(manager, associatedState)
         {
             gamemanager = manager;
+            gui = new RuminateGUI(gamemanager.game);
+            gui.SetTheme(new EmbeddedTheme(gui));
         }
 
         public override void LoadContent(ContentManager contentloader)
         {
+            start = new Button(new Offset(10, 120, 80, 100), "StartGame", null);
+            gui.AddElement(start);
 
+            gamemanager.game.IsMouseVisible = true;
         }
 
         public override void Update(GameTime gameTime, InputHelper input)
         {
+            gui.Update();
             if(input.IsNewPress(Keys.Enter))
+            {
+                gamemanager.Pbag.JoinGame();
+            }
+            if (start.IsPressed)
             {
                 gamemanager.Pbag.JoinGame();
             }
@@ -33,7 +48,8 @@ namespace MineWorld
 
         public override void Draw(GameTime gameTime)
         {
-            gamemanager.device.Clear(Color.Black);
+            gamemanager.device.Clear(Color.White);
+            gui.Draw();
         }
     }
 }
